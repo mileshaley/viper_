d42ffef7f6ceb77dfa70419f290c48c1f8f5b104
Miles Haley <71760141+mileshaley@users.noreply.github.com>
2025-02-20 14:48:31 -0800

    added cmakelists

--- include/viper_.h
+++ include/viper_.h
@@ -1,5 +1,20 @@
-
-
 #pragma once
+
+#include <unordered_map>
+#include <any>
+
+namespace viper {
+
+	class variable {
+	public:
+		variable();
+
+	private:
+		std::any m_data;
+
+	}; // class variable
+
+
+} // namespace viper
 
 
